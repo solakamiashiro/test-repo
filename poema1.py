38ab3e5b35137e9c436af943674016585885e3ab
Solange Kamiya Miashiro <kamiyasolange@hotmail.com>
2021-07-29 16:13:54 -0300

    add Augusto Cury poem

---
+++ poema1.py
@@ -0,0 +1,11 @@
+"Posso ter defeitos, viver ansioso e ficar irritado algumas vezes, mas não esqueço que minha vida é a maior empresa do mundo.
+ E que posso evitar que ela vá à falência.
+ Ser feliz é reconhecer que vale a pena viver, apesar de todos os desafios, incompreensões e períodos de crise.
+ Ser feliz é deixar de ser vítima dos problemas e se tornar autor da própria história.
+ É atravessar desertos fora de si, mas ser capaz de encontrar um oásis no recôndito da sua alma.
+ É agradecer a Deus a cada manhã pelo milagre da vida.
+ Ser feliz é não ter medo dos próprios sentimentos.
+ É saber falar de si mesmo. 
+ É ter coragem para ouvir um “não”.
+ É ter segurança para receber uma crítica, mesmo que injusta."
+(Augusto Cury)
